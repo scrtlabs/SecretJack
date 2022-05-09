cb2a01093599669cefb2af6291349493d59d11db
Lior Bondarevski <lior@scrtlabs.com>
2022-05-09 11:35:19 +0300

    Make the gui even more awsome

--- gui/src/components/Status.tsx
+++ gui/src/components/Status.tsx
@@ -3,21 +3,21 @@ import styles from './styles/Status.module.css';
 
 type StatusProps = {
   message: string,
-  balance: number,
-  address: string
+  bet: number,
+  balance: number
 };
 
-const Status: React.FC<StatusProps> = ({ message, balance, address }) => {
+const Status: React.FC<StatusProps> = ({ message, bet, balance }) => {
   return (
     <div className={styles.statusContainer}>
       <div className={styles.status}>
-        <h4 className={styles.value}>Playing as: {address}</h4>
+        <h4 className={styles.value}>Balance is: {balance} u𝕊</h4>
       </div>
-      <div className={styles.status}>
-        <h2 className={styles.value}>{message}</h2>
+      <div className={styles.message}>
+        <h2 className={styles.messageValue}>{message}</h2>
       </div>
-      <div className={styles.balance}>
-        <h1 className={styles.value}>Betting on: {balance} u𝕊</h1>
+      <div className={styles.bet}>
+        <h1 className={styles.betValue}>Betting on: {bet} u𝕊</h1>
       </div>
 
     </div>
